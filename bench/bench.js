2208b2e168e47eb4678b11b2b1870f0c805e5532
Samuel Ytterbrink <neppord@gmail.com>
2014-05-16 18:39:50 +0200

    changes the output of the benchmark to be more helpfull.

--- bench/bench.js
+++ bench/bench.js
@@ -4,11 +4,24 @@ var tokenize = require('../');
 var through = require('through2');
 
 var src = fs.readFileSync(__dirname + '/input.html');
-
+var reps = 3;
+var sets = 100
 function write (row, enc, next) { next(); }
 
 timeit(function (done){
     var t = tokenize();
     t.pipe(through.obj(write, done));
     t.end(src);
-}, 10, 10, console.log);
+}, sets, reps,
+function (timings) {
+  console.log(
+    "time spent in microseconds doing", reps ,"repetitions",
+    "and", sets, "sets" 
+    )
+  console.log("minimum time spent:", Math.min.apply(Math, timings))
+  console.log("maximum time spent:", Math.max.apply(Math, timings));
+  console.log(
+    "average time spent:",
+    timings.reduce(function (a, b){ return a + b }) / sets
+  );
+});
